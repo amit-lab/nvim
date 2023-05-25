5d0413398890f6db7a9ba0f4c1008f0b94a118db
Amit Dongarwar <amitdongarwar1810@gmail.com>
2023-05-25 16:04:59 +0530

    replacing coc-nvim with lsp-zero

--- lua/amit/packer.lua
+++ lua/amit/packer.lua
@@ -14,18 +14,34 @@ return require('packer').startup(function(use)
   }
 
   use({ 
-	  'rose-pine/neovim', 
-	  as = 'rose-pine',
-	  config = function()
-	  	vim.cmd('colorscheme rose-pine')
-	end
+      'rose-pine/neovim', 
+      as = 'rose-pine',
+      config = function()
+      	vim.cmd('colorscheme rose-pine')
+    end
   })
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('ThePrimeagen/harpoon')
   use {
-	  'neoclide/coc.nvim',
-	  branch = 'release',
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v2.x',
+      requires = {
+          -- LSP Support
+          {'neovim/nvim-lspconfig'},             -- Required
+          {                                      -- Optional
+          'williamboman/mason.nvim',
+          run = function()
+              pcall(vim.cmd, 'MasonUpdate')
+          end,
+      },
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'},     -- Required
+  }
   }
 
 end)
