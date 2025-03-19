20b0e818a96e005b700bbdfc98127fa883bf493d
amit-lab <amitdongarwar1810@gmail.com>
2025-03-19 20:23:48 +0530

    enabled transparency

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -31,6 +31,7 @@ return {
 	-- change the command in the config to whatever the name of that colorscheme is.
 	--
 	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+	-- old theme
 	"folke/tokyonight.nvim",
 	lazy = false,
 	priority = 1000, -- Make sure to load this before all the other start plugins.
@@ -43,4 +44,11 @@ return {
 		-- You can configure highlights by doing something like:
 		vim.cmd.hi("Comment gui=none")
 	end,
+	opts = {
+		transparent = true,
+		styles = {
+			sidebars = "transparent",
+			floats = "transparent",
+		},
+	},
 }
